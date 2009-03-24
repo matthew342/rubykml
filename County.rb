f05eccb78e3b43852879bbb4e18872f1d2e03778
aidan.s.marcuss <aidan.s.marcuss@1742b21e-943d-0410-9d9e-bda0ae528c44>
2009-03-24 16:15:04 +0000

    Adding GNU General Public License to each file.

--- County.rb
+++ County.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 #
 #  Created by Aidan Marcuss on 2007-03-19.
-#  Copyright (c) 2007. All rights reserved.
+#
+#  This file is part of RubyKML.
+#
+#  RubyKML is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  RubyKML is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with RubyKML.  If not, see <http://www.gnu.org/licenses/>.
 
 class County
   
